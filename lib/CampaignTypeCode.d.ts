d58a128e27bcfb05f6747fd1f20eaf419f037637
mottyc <motty.cohen@gmail.com>
2024-05-27 10:33:58 +0000

    Remove previous artifacts

--- lib/CampaignTypeCode.d.ts
+++
@@ -1,7 +0,0 @@
-export declare enum CampaignTypeCode {
-    UNDEFINED = 0,
-    WEB = 1,
-    EMAIL = 2,
-    TELESALES = 3
-}
-export declare function GetCampaignTypeCodes(): Map<CampaignTypeCode, string>;
