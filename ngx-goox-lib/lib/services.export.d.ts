dbcbf91d61868c137a148cb564334dbe964f20e5
mottyc <motty.cohen@gmail.com>
2024-05-27 09:11:37 +0000

    Add artifacts from build: 40

---
+++ ngx-goox-lib/lib/services.export.d.ts
@@ -0,0 +1,9 @@
+import { ContactsEndPoint } from '.';
+import { DocumentsEndPoint } from '.';
+import { LeadsEndPoint } from '.';
+import { UserEndPoint } from '.';
+import { UsersEndPoint } from '.';
+import { AccountsEndPoint } from '.';
+import { AuditLogsEndPoint } from '.';
+import { CampaignsEndPoint } from '.';
+export declare const Services: (typeof DocumentsEndPoint | typeof LeadsEndPoint | typeof UserEndPoint | typeof UsersEndPoint | typeof AccountsEndPoint | typeof AuditLogsEndPoint | typeof CampaignsEndPoint | typeof ContactsEndPoint)[];
