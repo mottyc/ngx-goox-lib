74f9ff73abb2b7d1ec0735c08803fbd5691172de
Motty Cohen <motty.cohen@gmail.com>
2024-05-27 12:05:13 +0300

    remove file

--- client_lib/ng-workspace/dist/ngx-goox-lib/lib/CampaignStatusCode.d.ts
+++
@@ -1,8 +0,0 @@
-export declare enum CampaignStatusCode {
-    UNDEFINED = 0,
-    PLANNING = 1,
-    ACTIVE = 2,
-    COMPLETED = 3,
-    SUSPENDED = 4
-}
-export declare function GetCampaignStatusCodes(): Map<CampaignStatusCode, string>;
