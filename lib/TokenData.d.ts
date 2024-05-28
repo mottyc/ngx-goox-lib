0db36574c1754fdf0639592ec2694cf5ab1631fa
mottyc <motty.cohen@gmail.com>
2024-05-28 06:40:11 +0000

    Remove previous artifacts

--- lib/TokenData.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserStatusCode } from '.';
-import { UserTypeCode } from '.';
-export declare class TokenData {
-    subjectId: string;
-    subjectType: UserTypeCode;
-    status: UserStatusCode;
-    expiresIn: number;
-    constructor(subjectId?: string, subjectType?: UserTypeCode, status?: UserStatusCode, expiresIn?: number);
-}
--- utils/ActionResponse.d.ts
+++
@@ -1,5 +0,0 @@
-import { BaseRestResponse } from './BaseRestResponse';
-export declare class ActionResponse extends BaseRestResponse {
-    key: string;
-    data: string;
-}
