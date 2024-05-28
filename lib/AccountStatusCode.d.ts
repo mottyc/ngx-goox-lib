e8d46c53f67b0e38747e3496b8cecad3f91c955a
mottyc <motty.cohen@gmail.com>
2024-05-28 07:06:37 +0000

    Remove previous artifacts

--- lib/AccountStatusCode.d.ts
+++
@@ -1,8 +0,0 @@
-export declare enum AccountStatusCode {
-    UNDEFINED = 0,
-    ACTIVE = 1,
-    INACTIVE = 2,
-    BLOCKED = 3,
-    SUSPENDED = 4
-}
-export declare function GetAccountStatusCodes(): Map<AccountStatusCode, string>;
--- lib/Document.d.ts
+++
@@ -1,9 +0,0 @@
-import { BaseEntity } from '.';
-export declare class Document extends BaseEntity {
-    name: string;
-    url: string;
-    createdBy: string;
-    activeFrom: number;
-    activeUntil: number;
-    category: string;
-}
