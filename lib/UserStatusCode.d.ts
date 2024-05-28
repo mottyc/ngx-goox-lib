c70da26a7d1767ff667344396ceb74f649204dcf
mottyc <motty.cohen@gmail.com>
2024-05-28 14:08:10 +0000

    Remove previous artifacts

--- lib/UserStatusCode.d.ts
+++
@@ -1,8 +0,0 @@
-export declare enum UserStatusCode {
-    UNDEFINED = 0,
-    PENDING = 1,
-    ACTIVE = 2,
-    BLOCKED = 3,
-    SUSPENDED = 4
-}
-export declare function GetUserStatusCodes(): Map<UserStatusCode, string>;
