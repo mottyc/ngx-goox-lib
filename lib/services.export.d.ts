357d5aeb6324e13090d59648617a2bb1c2d3b110
mottyc <motty.cohen@gmail.com>
2024-07-07 11:16:58 +0000

    Remove previous artifacts

--- lib/services.export.d.ts
+++
@@ -1,9 +0,0 @@
-import { UsersEndPoint } from '.';
-import { AccountsEndPoint } from '.';
-import { AuditLogsEndPoint } from '.';
-import { CampaignsEndPoint } from '.';
-import { ContactsEndPoint } from '.';
-import { DocumentsEndPoint } from '.';
-import { LeadsEndPoint } from '.';
-import { UserEndPoint } from '.';
-export declare const Services: (typeof AccountsEndPoint | typeof AuditLogsEndPoint | typeof CampaignsEndPoint | typeof ContactsEndPoint | typeof DocumentsEndPoint | typeof LeadsEndPoint | typeof UserEndPoint | typeof UsersEndPoint)[];
