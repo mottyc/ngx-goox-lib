ac4c36e0e84b51ce09895c292364f24e3b7d4d12
mottyc <motty.cohen@gmail.com>
2024-07-23 07:12:02 +0000

    Add artifacts from build: 86

---
+++ lib/services.export.d.ts
@@ -0,0 +1,9 @@
+import { DocumentsEndPoint } from '.';
+import { LeadsEndPoint } from '.';
+import { UserEndPoint } from '.';
+import { UsersEndPoint } from '.';
+import { AccountsEndPoint } from '.';
+import { AuditLogsEndPoint } from '.';
+import { CampaignsEndPoint } from '.';
+import { ContactsEndPoint } from '.';
+export declare const Services: (typeof AuditLogsEndPoint | typeof CampaignsEndPoint | typeof ContactsEndPoint | typeof DocumentsEndPoint | typeof LeadsEndPoint | typeof UserEndPoint | typeof UsersEndPoint | typeof AccountsEndPoint)[];
