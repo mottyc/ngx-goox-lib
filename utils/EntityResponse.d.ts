784f49586166716a9734012f70af7484e1c2d14b
mottyc <motty.cohen@gmail.com>
2024-07-24 06:13:28 +0000

    Remove previous artifacts

--- utils/EntityResponse.d.ts
+++
@@ -1,8 +0,0 @@
-import { BaseRestResponse } from './BaseRestResponse';
-export declare class EntityResponse<T> extends BaseRestResponse {
-    page: number;
-    size: number;
-    pages: number;
-    total: number;
-    entity: T;
-}
