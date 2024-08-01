813f7b7c885b84726db67dde73ca017e4d6392b3
mottyc <motty.cohen@gmail.com>
2024-08-01 05:54:29 +0000

    Remove previous artifacts

--- lib/Billing.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Billing {
-    status: number;
-    units: number;
-    unitPrice: number;
-    description: string;
-    totalCharge: number;
-    totalChargeWithVat: number;
-    constructor(status?: number, units?: number, unitPrice?: number, description?: string, totalCharge?: number, totalChargeWithVAT?: number);
-}
