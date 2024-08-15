2cb703bcf2564ccd3aa2208223d7fa9428ab509b
mottyc <motty.cohen@gmail.com>
2024-08-15 17:22:49 +0000

    Remove previous artifacts

--- lib/SpecialRisks.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class SpecialRisks {
-    specialFlightPermit: number;
-    specialMelCdlItems: number;
-    specialFlightLimitAfm: number;
-    constructor(specialFlightPermit?: number, specialMelCdlItems?: number, specialFlightLimitAfm?: number);
-}
