8fce8c127b521260ca53c26ba08efc68fb2d7bb3
mottyc <motty.cohen@gmail.com>
2024-08-20 12:30:00 +0000

    Add artifacts from build: 98

---
+++ lib/Maintenance.d.ts
@@ -0,0 +1,20 @@
+import { MaintenanceActionCode } from '.';
+import { BaseEntity } from '.';
+import { StatusCode } from '.';
+export declare class Maintenance extends BaseEntity {
+    name: string;
+    description: string;
+    airplaneId: string;
+    documentId: string;
+    hobs: number;
+    nextHobs: number;
+    tach: number;
+    nextTach: number;
+    validUntil: number;
+    status: StatusCode;
+    component: string;
+    action: MaintenanceActionCode;
+    notifyAt: number;
+    subject: string;
+    message: string;
+}
