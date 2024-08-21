4da0238463876b52b2239a6cc81c5e918c38fe74
mottyc <motty.cohen@gmail.com>
2024-08-21 13:43:48 +0000

    Remove previous artifacts

--- lib/OnBoard.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class OnBoard {
-    captain: string;
-    firstOfficer: string;
-    passengers: string[];
-    crew: string[];
-    constructor(captain?: string, firstOfficer?: string, passengers?: string[], crew?: string[]);
-}
