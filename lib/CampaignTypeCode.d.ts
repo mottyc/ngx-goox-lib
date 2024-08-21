5463e3cd0089236ea2b5e5875647595be714a388
mottyc <motty.cohen@gmail.com>
2024-08-21 07:57:25 +0000

    Remove previous artifacts

--- lib/CampaignTypeCode.d.ts
+++
@@ -1,9 +0,0 @@
-import { Tuple } from '.';
-export declare enum CampaignTypeCode {
-    UNDEFINED = 0,
-    WEB = 1,
-    EMAIL = 2,
-    TELESALES = 3
-}
-export declare function GetCampaignTypeCodes(): Tuple<CampaignTypeCode, string>[];
-export declare function MapCampaignTypeCodes(): Map<CampaignTypeCode, string>;
--- lib/TokenData.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserTypeCode } from '.';
-import { UserStatusCode } from '.';
-export declare class TokenData {
-    subjectId: string;
-    subjectType: UserTypeCode;
-    status: UserStatusCode;
-    expiresIn: number;
-    constructor(subjectId?: string, subjectType?: UserTypeCode, status?: UserStatusCode, expiresIn?: number);
-}
