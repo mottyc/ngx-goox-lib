adbc49e080636cd145d34f4fa0929d08aad92f3b
mottyc <motty.cohen@gmail.com>
2024-08-22 15:04:25 +0000

    Remove previous artifacts

--- lib/TripRisks.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class TripRisks {
-    twilightOperation: number;
-    nightTimeOperation: number;
-    repositioningFlight: number;
-    popupTripLess4HrsNotice: number;
-    internationalTrip: number;
-    constructor(twilightOperation?: number, nightTimeOperation?: number, repositioningFlight?: number, popupTripLess4HrsNotice?: number, internationalTrip?: number);
-}
