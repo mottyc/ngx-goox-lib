5001b4f37b33f22242b1402710d17cf64caaef0c
mottyc <motty.cohen@gmail.com>
2024-09-07 16:36:09 +0000

    Remove previous artifacts

--- lib/ColumnDef.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class ColumnDef {
-    name: string;
-    type: string;
-    sort: number;
-    filterOp: number;
-    filter: number | number[] | string | string[];
-    constructor(name?: string, type?: string, sort?: number, filterOp?: number, filter?: number | number[] | string | string[]);
-}
