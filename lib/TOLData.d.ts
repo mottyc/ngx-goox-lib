387517f2360f1f7f14666091488cf68831bdf9eb
mottyc <motty.cohen@gmail.com>
2024-09-07 10:01:29 +0000

    Remove previous artifacts

--- lib/TOLData.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class TOLData {
-    planned: number;
-    time: number;
-    hobs: number;
-    tach: number;
-    field: string;
-    constructor(planned?: number, time?: number, hobs?: number, tach?: number, field?: string);
-}
