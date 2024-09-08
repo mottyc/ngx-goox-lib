8e7633d2151f217e3caafb772b507c741f32add8
mottyc <motty.cohen@gmail.com>
2024-09-08 10:03:13 +0000

    Remove previous artifacts

--- lib/SystemConfig.d.ts
+++
@@ -1,7 +0,0 @@
-import { BaseEntity } from '.';
-import { ColumnDef } from '.';
-export declare class SystemConfig extends BaseEntity {
-    strVal: string;
-    intVal: number;
-}
-export declare function GetSystemConfigColumnsDef(): ColumnDef[];
