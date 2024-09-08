b82a29b328367875d9ca466913c0fbf54f5379e9
mottyc <motty.cohen@gmail.com>
2024-09-08 12:59:40 +0000

    Remove previous artifacts

--- lib/OperatorCode.d.ts
+++
@@ -1,5 +0,0 @@
-import { Tuple } from '.';
-export declare enum OperatorCode {
-}
-export declare function GetOperatorCodes(): Tuple<OperatorCode, string>[];
-export declare function MapOperatorCodes(): Map<OperatorCode, string>;
--- lib/SystemConfig.d.ts
+++
@@ -1,7 +0,0 @@
-import { ColumnDef } from '.';
-import { BaseEntity } from '.';
-export declare class SystemConfig extends BaseEntity {
-    strVal: string;
-    intVal: number;
-}
-export declare function GetSystemConfigColumnsDef(): ColumnDef[];
