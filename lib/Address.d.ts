baef91639d51c6597caecb7c7f577b8ac0ad29cc
mottyc <motty.cohen@gmail.com>
2024-09-30 11:41:09 +0000

    Remove previous artifacts

--- lib/Address.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Address {
-    street: string;
-    city: string;
-    state: string;
-    zipCode: string;
-    country: string;
-    constructor(street?: string, city?: string, state?: string, zipCode?: string, country?: string);
-}
