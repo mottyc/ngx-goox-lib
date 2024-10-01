92eecdb06d5a425671eca58e8f5306a55232574c
mottyc <motty.cohen@gmail.com>
2024-10-01 13:35:09 +0000

    Remove previous artifacts

--- lib/DepartureRisks.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class DepartureRisks {
-    mountainousAirport: number;
-    towerNotOperationalAtEtd: number;
-    nonToweredAirport: number;
-    wetRunway: number;
-    contaminatedRunway: number;
-    constructor(mountainousAirport?: number, towerNotOperationalAtEtd?: number, nonToweredAirport?: number, wetRunway?: number, contaminatedRunway?: number);
-}
--- lib/SystemConfig.d.ts
+++
@@ -1,8 +0,0 @@
-import { BaseEntityEx } from '.';
-import { ColumnDef } from '.';
-export declare class SystemConfig extends BaseEntityEx {
-    strVal: string;
-    intVal: number;
-    prop(field: string): any;
-}
-export declare function GetSystemConfigColumnsDef(): ColumnDef[];
