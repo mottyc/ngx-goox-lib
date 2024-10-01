422af8b0a3002eea0c957b0cdb0c70f279cb3793
mottyc <motty.cohen@gmail.com>
2024-10-01 13:17:45 +0000

    Remove previous artifacts

--- lib/Enums.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function MapEnumValues(): Map<string, string>;
--- lib/SystemConfig.d.ts
+++
@@ -1,7 +0,0 @@
-import { BaseEntityEx } from '.';
-import { ColumnDef } from '.';
-export declare class SystemConfig extends BaseEntityEx {
-    strVal: string;
-    intVal: number;
-}
-export declare function GetSystemConfigColumnsDef(): ColumnDef[];
