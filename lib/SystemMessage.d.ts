edb31ba9f0b7a90842ce71b7cb581c8caeb16e90
mottyc <motty.cohen@gmail.com>
2024-10-15 13:25:30 +0000

    Add artifacts from build: 140

---
+++ lib/SystemMessage.d.ts
@@ -0,0 +1,13 @@
+import { BaseEntityEx } from '.';
+import { ColumnDef } from '.';
+import { MessageTypeCode } from '.';
+import { PriorityCode } from '.';
+export declare class SystemMessage extends BaseEntityEx {
+    subject: string;
+    description: string;
+    type: MessageTypeCode;
+    priority: PriorityCode;
+    groups: string[];
+    get(field: string): any;
+}
+export declare function GetSystemMessageColumnsDef(): ColumnDef[];
