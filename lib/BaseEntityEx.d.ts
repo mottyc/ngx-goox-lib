1785882934ac1d6c9b6530d25e4cb520155c6f60
mottyc <motty.cohen@gmail.com>
2024-10-15 13:25:30 +0000

    Remove previous artifacts

--- lib/BaseEntityEx.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class BaseEntityEx {
-    id: string;
-    createdOn: number;
-    updatedOn: number;
-    props: Map<string, object>;
-    constructor(id?: string, createdOn?: number, updatedOn?: number, props?: Map<string, object>);
-}
