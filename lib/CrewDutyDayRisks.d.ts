d8bf554b40eda60e46c5c5ddfe3866653c45c299
mottyc <motty.cohen@gmail.com>
2024-10-16 06:04:22 +0000

    Remove previous artifacts

--- lib/CrewDutyDayRisks.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class CrewDutyDayRisks {
-    crewDutyDayOver12Hrs: number;
-    flightTimeOver8Hrs: number;
-    crewRestLess10Hrs: number;
-    constructor(crewDutyDayOver12Hrs?: number, flightTimeOver8Hrs?: number, crewRestLess10Hrs?: number);
-}
--- lib/TimeFrame.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class TimeFrame {
-    from: number;
-    to: number;
-    constructor(from?: number, to?: number);
-}
