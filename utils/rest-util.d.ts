f4abe4ae22446c741a0ec2fd5e092e69ed4abdbf
mottyc <motty.cohen@gmail.com>
2024-10-21 12:26:50 +0000

    Add artifacts from build: 144

---
+++ utils/rest-util.d.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import * as i0 from "@angular/core";
+export declare class RestUtil {
+    private http;
+    constructor(http: HttpClient);
+    upload<T>(file: File, url: string, ...params: string[]): Observable<import("@angular/common/http").HttpEvent<T>>;
+    download(fileName: string, url: string, ...params: string[]): Observable<import("@angular/common/http").HttpEvent<Blob>>;
+    download2(fileName: string, url: string, ...params: string[]): void;
+    get<T>(url: string, ...params: string[]): Observable<T>;
+    post<T>(url: string, body?: string, ...params: string[]): Observable<T>;
+    put<T>(url: string, body?: string, ...params: string[]): Observable<T>;
+    patch<T>(url: string, body?: string, ...params: string[]): Observable<T>;
+    delete<T>(url: string, ...params: string[]): Observable<T>;
+    private buildUrl;
+    private getMimeType;
+    static ɵfac: i0.ɵɵFactoryDeclaration<RestUtil, never>;
+    static ɵprov: i0.ɵɵInjectableDeclaration<RestUtil>;
+}
