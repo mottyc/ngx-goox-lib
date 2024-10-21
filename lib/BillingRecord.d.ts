639bb1713f3caa2b8bd289c687ee1d88f33b3be6
mottyc <motty.cohen@gmail.com>
2024-10-21 12:26:50 +0000

    Remove previous artifacts

--- lib/BillingRecord.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class BillingRecord {
-    units: number;
-    unitPrice: number;
-    description: string;
-    exempt: boolean;
-    constructor(units?: number, unitPrice?: number, description?: string, exempt?: boolean);
-}
